56cf75c4a892bd063ef71a82556e7dc5547a4cd1
Hieu Tran <hieutran106@gmail.com>
2018-03-31 11:31:30 +1000

    finish pagination

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -15,7 +16,23 @@ namespace SportsStore.Controllers
         {
             this.repo = repo;
         }
-        public ViewResult List(int page=1) 
-            => View(repo.Products.OrderBy(p => p.ProductID).Skip((page-1)*PageSize).Take(PageSize));
+        public ViewResult List(int page=1)
+        {
+            IEnumerable<Product> products = repo.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize);
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = repo.Products.Count()
+
+            };
+            ProductListViewModel viewModel = new ProductListViewModel
+            {
+                Products = products,
+                PagingInfo = pagingInfo
+            };
+            return View(viewModel);
+        }
+            
     }
 }
