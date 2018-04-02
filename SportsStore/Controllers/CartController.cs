41f38de7e6f91f2faa222e9f83cd9beeebf2f175
Hieu Tran <hieutran106@gmail.com>
2018-04-02 10:21:22 +1000

    Implement Cart Controller and session state

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private IProductRepository repo;
+        public CartController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repo.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (product!=null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repo.Products
+                                .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
